607db49f49dc0170c407868f5aa07053ddc456d3
Justin Le Tourneau <justin.letourn@gmail.com>
2017-03-03 17:04:53 -0600

    added recaptcha support. added more sponsors and fixed submission

--- src/submit_application.php
+++ src/submit_application.php
@@ -1,12 +1,29 @@
 <?php
 
-if (isset($_POST['submit'])) {
+    function verifyCapcha($capcha, $secret_key){
+        $url = 'https://www.google.com/recaptcha/api/siteverify';
+        $data = array(
+            'secret' => $secret_key,
+            'response' => $capcha
+        );
+        $options = array(
+            'http' => array (
+                'method' => 'POST',
+                'content' => http_build_query($data)
+            )
+        );
+        $context  = stream_context_create($options);
+        $verify = file_get_contents($url, false, $context);
+        $captcha_success = json_decode($verify);
+
+        return $captcha_success->success;
+    }
 
     $google_capcha = '6Le5chcUAAAAAMymO7ubZVMb8CLNp9ePCI6n55OA';
 
     $result = (object) array(
         'success' => false,
-        'message' => ''
+        'message' => "Application could not be submitted. Please try again."
     );
 
     // lets set us up a connection
@@ -21,9 +38,9 @@ if (isset($_POST['submit'])) {
     $stmt = $mysqli->prepare("INSERT INTO student_applications (first_name, last_name, email, street, city, school_name, school_district, shirt_size, allergies, post_highschool_plans, modivation, as_background, tour_interest) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)");
     // bind params, and i guess pretend to be a sssssssnake
     $stmt->bind_param('sssssssssssss', $first_name,
-                                       $last_name
+                                       $last_name,
                                        $email,
-                                       $street
+                                       $street,
                                        $city,
                                        $school_name,
                                        $school_district,
@@ -35,30 +52,36 @@ if (isset($_POST['submit'])) {
                                        $tour_interest);
 
     if ($_SERVER['REQUEST_METHOD'] == 'POST' && !empty($_POST)) {
-        // lets get all of the submitted 
-        $first_name = isset($_POST['first_name']) ? $_POST['first_name'] : null;
-        $last_name = isset($_POST['last_name']) ? $_POST['last_name'] : null;
-        $email = isset($_POST['email']) ? $_POST['email'] : null;
-        $street = isset($_POST['street']) ? $_POST['street'] : null;
-        $city = isset($_POST['city']) ? $_POST['city'] : null;
-        $school_name = isset($_POST['school_name']) ? $_POST['school_name'] : null;
-        $school_district = isset($_POST['school_district']) ? $_POST['school_district'] : null;
-        $shirt_size = isset($_POST['shirt_size']) ? $_POST['shirt_size'] : null;
-        $post_highschool_plans = isset($_POST['career_goals']) ? $_POST['career_goals'] : null;
-        $modivation = isset($_POST['modivation']) ? $_POST['modivation'] : null;
-        $as_background = isset($_POST['as_background']) ? $_POST['as_background'] : null;
-        $tour_interest = isset($_POST['tour_interest']) ? $_POST['tour_interest'] : null;
-        $allergies = isset($_POST['allergies']) ? $_POST['allergies'] : null;
 
-        /* execute prepared statement */
-        $statement_success = $stmt->execute();
+        $capcha = isset($_POST['capcha']) ? $_POST['capcha'] : null;
 
-        if($statement_success){
-            $result['success'] = true;
-            $result['message'] = "Application submitted successfully!";
+
+        if(verifyCapcha($capcha, $google_capcha)){
+
+            // lets get all of the submitted 
+            $first_name = isset($_POST['first_name']) ? $_POST['first_name'] : null;
+            $last_name = isset($_POST['last_name']) ? $_POST['last_name'] : null;
+            $email = isset($_POST['email']) ? $_POST['email'] : null;
+            $street = isset($_POST['street']) ? $_POST['street'] : null;
+            $city = isset($_POST['city']) ? $_POST['city'] : null;
+            $school_name = isset($_POST['school_name']) ? $_POST['school_name'] : null;
+            $school_district = isset($_POST['school_district']) ? $_POST['school_district'] : null;
+            $shirt_size = isset($_POST['shirt_size']) ? $_POST['shirt_size'] : null;
+            $post_highschool_plans = isset($_POST['career_goals']) ? $_POST['career_goals'] : null;
+            $modivation = isset($_POST['modivation']) ? $_POST['modivation'] : null;
+            $as_background = isset($_POST['as_background']) ? $_POST['as_background'] : null;
+            $tour_interest = isset($_POST['tour_interest']) ? $_POST['tour_interest'] : null;
+            $allergies = isset($_POST['allergies']) ? $_POST['allergies'] : null;
+
+            /* execute prepared statement */
+            $statement_success = $stmt->execute();
+
+            if($statement_success){
+                $result->success = true;
+                $result->message = "Application submitted successfully!";
+            }
         } else {
-            $result['success'] = false;
-            $result['message'] = "Application could not be submitted. Please try again.";
+            $result->message = "Google says you're a robot";
         }
 
         //printf("%d Row inserted.\n", $stmt->affected_rows);
@@ -70,7 +93,9 @@ if (isset($_POST['submit'])) {
     /* close connection */
     $mysqli->close();
 
-    echo json_encode($result)
-}
+    ob_clean();
+    
+    header('Content-Type: application/json');
+    echo json_encode($result);
 
 ?>
